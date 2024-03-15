a0057cf2a6f98e9616570daab4647c499dcd8738
Tanmay Shah <tanmay.shah@toothsi.in>
2024-03-15 17:49:32 +0530

    header footer fix added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,28 @@
 // Header.js
+'use client';
 import styles from './Header.module.scss';
-
+import { useState, useEffect } from 'react';
 function Header() {
+  // const [isSticky, setIsSticky] = useState(false);
+
+  // const handleScroll = () => {
+  //   const offset = window.scrollY;
+  //   if (offset > 0 && !isSticky) {
+  //     setIsSticky(true);
+  //   } else if (offset === 0 && isSticky) {
+  //     setIsSticky(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} `}>
       <div className={styles.logo}>Online Dawakhana</div>
       <nav className={styles.menu}>{/* Menu items */}</nav>
     </header>
